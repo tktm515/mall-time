342617b50f09a9d12932c0cb71a12034d7f072e0
tktm515 <tk.tm.515@gmail.com>
2021-03-17 16:06:54 +0900

    出品機能の実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,9 +1,35 @@
 class FoodsController < ApplicationController
 
+<<<<<<< Updated upstream
 
   private
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+=======
+  def index
   end
+
+  def new
+    @foods = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  
+
+  private
+
+  def food_params
+    params.permit(:food_name, :food_text, :price, :image).merge(tenant_id: current_tenant.id)
+>>>>>>> Stashed changes
+  end
+
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,17 @@
 class Food < ApplicationRecord
   has_one_attached :image
+<<<<<<< Updated upstream
+=======
+  has_many :reserves
+  belongs_to :tenant
+
+  
+  with_options presence: true do
+    validates :image
+    validates :food_name
+    validates :price
+    validates :food_text
+    validates :tenant_id
+  end
+>>>>>>> Stashed changes
 end
